3388cbc0c57e54efb64015061ee908508fa05fa4
@christian.gonzalez2 <christian.gonzalez@hireborderless.com>
2023-07-25 21:45:18 -0600

    info consumed from api

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,13 +1,21 @@
-import { Item } from "../Item";
+import { useEffect, useState } from "react";
 import { Items } from "../Items";
+import { ItemApi, fetchItems } from "../../api/api";
 
 export const Content = () => {
-  const items: Array<Item> = [
-    { name: "iPhone", price: 100 },
-    { name: "Keyboard", price: 200 },
-    { name: "Mouse", price: 75 },
-    { name: "PS5", price: 750 },
-  ];
+  // We need a state for the items
+  const [items, setItems] = useState<ItemApi[]>([]); // Initialiced as an empty array
+
+  useEffect(() => {
+    const fetchItemsFromApi = async () => {
+      const fetchedItems = await fetchItems(); // we use our custom function
+      setItems(fetchedItems); // update the state
+    };
+
+    fetchItemsFromApi(); // constantly call this while page loads
+  }, []);
+
+  console.log("DEBUG fetchedItems ", items);
   return (
     <>
       <h1>Here we are going to list the elements</h1>
--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,17 +1,38 @@
 import { nanoid } from "nanoid";
+import { ItemApi } from "../../api/api";
+import { Button, Card } from "antd";
+import { CustomCard } from "./stylesheets";
+import "./stylesheets";
 
-interface ItemProps {
-  name: string;
-  price: number;
+// Interface IS used, but from a different file
+interface ItemComponentProps {
+  item: ItemApi;
 }
-export type Item = {
-  name: string;
-  price: number;
-};
-export const ItemComponent: React.FC<ItemProps> = ({ name, price }) => {
+
+export const ItemComponent: React.FC<ItemComponentProps> = ({ item }) => {
+  const { id, title, price, description, category, image, rating } = item;
+  const handleClick = () => {
+    return alert("Added to cart!");
+  };
+
   return (
-    <li key={`${name}+${nanoid()}`}>
-      {name} ${price}
-    </li>
+    <CustomCard
+      hoverable
+      style={{ width: "240" }}
+      cover={<img src={image} alt={title} className="image" />}
+    >
+      <h3>{title}</h3>
+      <p>Price: ${price}</p>
+      <p>Category: {category}</p>
+      <p>Rating: {rating.rate}</p>
+      <p className="description">Description: {description}</p>
+      <Button onClick={handleClick} type="primary">
+        Add to Cart
+      </Button>
+    </CustomCard>
   );
 };
+
+// <li key={id}>
+//       {title} ${price}
+//     </li>
